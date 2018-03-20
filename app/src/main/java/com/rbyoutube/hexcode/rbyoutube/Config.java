d993b9e773597d28b76cdacb1e71f32fe86d4789
rishad <rishadb7@gmail.com>
2018-03-20 16:06:07 +0530

    Initial

---
+++ app/src/main/java/com/rbyoutube/hexcode/rbyoutube/Config.java
@@ -0,0 +1,13 @@
+package com.rbyoutube.hexcode.rbyoutube;
+
+/**
+ * Created by rishad on 20/3/18.
+ */
+
+public class Config {
+
+    public static final String DEVELOPER_KEY = "AIzaSyAgKe5Mrr3ff5h4gRRtEc_DGaYXL_qOdI0";
+    public static final String YOUTUBE_VIDEO_CODE = "pC2yUlN2kMU";//pC2yUlN2kMU  _oEA18Y8gM0
+
+    public static final String videos[]={"","","","",""};
+}
---
+++ app/src/main/java/com/rbyoutube/hexcode/rbyoutube/MainActivity.java
@@ -0,0 +1,76 @@
+package com.rbyoutube.hexcode.rbyoutube;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubePlayer.PlayerStyle;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
+    Button pauseBtn;
+    private YouTubePlayer mPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        pauseBtn=(Button)findViewById(R.id.pauseBtn);
+
+        youTubeView.initialize(Config.DEVELOPER_KEY, this);
+
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            String errorMessage = String.format(
+                    getString(R.string.error_player), errorReason.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                        YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+
+
+            player.loadVideo(Config.YOUTUBE_VIDEO_CODE);
+
+            player.setPlayerStyle(PlayerStyle.DEFAULT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            getYouTubePlayerProvider().initialize(Config.DEVELOPER_KEY, this);
+        }
+    }
+
+    private YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView) findViewById(R.id.youtube_view);
+    }
+}
